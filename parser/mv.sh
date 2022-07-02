1d23a8ee4e834aafdaf8559c4351293f41cf9c1d
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2022-07-02 11:57:46 +0200

    Rename

---
+++ parser/mv.sh
@@ -0,0 +1,13 @@
+mv -v ./hakibenita/hakibenita.go ./hakibenita/parser.go
+mv -v ./benoitessiambre/benoitessiambre.go ./benoitessiambre/parser.go
+mv -v ./aykevl/aykevl.go ./aykevl/parser.go
+mv -v ./atom/atom.go ./atom/parser.go
+mv -v ./monzo/monzo.go ./monzo/parser.go
+mv -v ./rss/rss.go ./rss/parser.go
+mv -v ./hakibenita/hakibenita_test.go ./hakibenita/parser_test.go
+mv -v ./benoitessiambre/benoitessiambre_test.go ./benoitessiambre/parser_test.go
+mv -v ./aykevl/aykevl_test.go ./aykevl/parser_test.go
+mv -v ./atom/atom_test.go ./atom/parser_test.go
+mv -v ./monzo/monzo_test.go ./monzo/parser_test.go
+mv -v ./rss/rss_test.go ./rss/parser_test.go
+
